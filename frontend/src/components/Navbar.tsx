2432cba92a2801775ab6cd3e8fe5b295da473c6e
Mattan-Sania <saniaamattan@gmail.com>
2026-06-17 15:03:22 +0200

    feat: implement Home page with Navbar and initial content; add Navbar component and styles

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+function Navbar() {
+    return (
+        <nav>
+            <div className="nav-links">
+                <a href="/">Home</a>
+                <a href="/market">Market</a>
+                <a href="/about">About</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar"
+
+function Home() {
+    return (
+        <>
+            <Navbar />
+            <main>
+                <h1>QueueCup</h1>
+                <p>A dynamic café reservation and pricing sytem.</p>
+            </main>
+        </>
+    )
+}
+
+export default Home
