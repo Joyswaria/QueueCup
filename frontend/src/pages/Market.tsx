ffe87b7692220c9fa8438e64e491144f567e8339
Mattan-Sania <saniaamattan@gmail.com>
2026-06-17 16:44:05 +0200

    feat: display menu items in Market page; enhance layout with list structure

--- frontend/src/pages/Market.tsx
+++ frontend/src/pages/Market.tsx
@@ -1,15 +1,31 @@
 import Navbar from "../components/Navbar"
+import { menuItems } from "../data/menuItems"
 
 function Market() {
-    return (
-        <>
-            <Navbar />
-                <main>
-                    <h1>Live café Market</h1>
-                    <p>Menu items will be displayed here.</p>
-                </main>
-        </>
-    )
+  return (
+    <>
+      <Navbar />
+
+      <main>
+        <h1>Live Café Market</h1>
+        <p>Menu items will be displayed here.</p>
+
+        <ul>
+            {menuItems.map((item) => (
+                <li key={item.id}>
+                    <h2>{item.name}</h2>
+                    <p><strong>{item.description}</strong></p>
+                    <p>{item.category}</p>
+                    <p>{item.currentPrice}</p>
+                    <p>{item.demand}</p>
+                    <p>{item.volume}</p>
+                </li>
+            ))}
+        </ul>
+
+      </main>
+    </>
+  )
 }
 
 export default Market
